8f4234ccc41e49ace02e8c311810d4b0db2cce8b
Amar kumar <kumar.amar2050l@gmail.com>
2026-02-07 19:58:48 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Empty list
+my_list = []
+
+# List with items
+fruits = ["apple", "banana", "orange"]
+numbers = [1, 2, 3, 4, 5]
+mixed = ["hello", 42, True, 3.14]  # Different types OK!
+
+my_list2=["Amar",2,3,"kumar"]
+
+print(my_list.append("hello"))
